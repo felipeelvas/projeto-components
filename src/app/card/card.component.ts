73508d0edf327ca12e731d5167f7adfef35d2ab6
Felipe Elvas Barbosa <felipeelvas+1@gmail.com>
2024-06-13 11:13:56 -0300

    versionando

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+plano = {
+  infos: {
+    tipo: 'Simples',
+    preco: 100,
+  },
+};
+
+}
