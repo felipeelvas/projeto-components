3013b946c395cd09f5a5f28a777ccc5c183dde1b
Felipe Elvas Barbosa <felipeelvas+1@gmail.com>
2024-06-29 07:42:47 -0300

    Classroom output

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
+  propTest: boolean = false;
+  @Output('buttonClick') buttonClickedEmitter = new EventEmitter<boolean>();
+
+onButtonClick(){
+  console.log('onButtonClick')
+
+  this.buttonClickedEmitter.emit(this.propTest);
+}
 
 }
